de95d7289a5cbb82864767122e5ac4df37f17ec0
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-11-10 17:49:05 +0100

    update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ RUST_BIN="./target/${1:=release}/gors"
 GO_BIN='./go/go'
 
 # read the last file tested (if any)
-last="$(cat .tests/_last)"
+last="$(cat .tests/_last 2>/dev/null)"
 
 # prepare the working directory
 rm -rf ".tests/"
@@ -14,10 +14,10 @@ mkdir ".tests/"
 
 # define the go files we want to test against
 if [ "$2" = 'last' ]; then
-  echo "$last" > .tests/_index
+  echo "$last" >.tests/_index
 else
   for dir in tests .repositories; do
-    find "$dir" -name '*.go' | sort > .tests/_index
+    find "$dir" -type f -name '*.go' | sort > .tests/_index
   done
 fi
 
@@ -27,18 +27,27 @@ total="$(wc -l < .tests/_index)"
 
 # compare tokens generated with the Go/Rust implementation
 while read go_source; do
-  go_tokens=".tests/${go_source//\//--}.tokens-go"
-  "$GO_BIN" tokens "$go_source" > "$go_tokens" 2>/dev/null || continue
+  i=$((i+1))
 
   echo "$go_source" > .tests/_last
-  printf "\r%0.2d%% %s" "$((i*100/$total))" "$go_source"
+  printf "%0.2d%% %s\n" "$((i*100/$total))" "$go_source"
+
+  go_tokens=".tests/${go_source//\//--}.tokens-go"
+  "$GO_BIN" tokens "$go_source" > "$go_tokens" || {
+    echo "$go_source" >> .tests/_failures
+    continue
+  }
 
   rust_tokens=".tests/${go_source//\//--}.tokens-rust"
   "$RUST_BIN" tokens "$go_source" > "$rust_tokens" || exit 1
 
   git diff --no-index "$go_tokens" "$rust_tokens" || exit 2
-
-  i=$((i+1))
 done < .tests/_index
 
-echo 'Done!'
+echo 'Success!'
+
+if [ -r .tests/_failures ]; then
+  echo
+  echo 'WARNING: these files were skipped as the Go parser failed to process them:'
+  cat .tests/_failures
+fi
