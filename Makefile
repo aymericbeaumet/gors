4620b6f0554d5f6139a6abe59eb31a1825b0a55c
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-11-11 11:28:28 +0100

    improve test setup to support ast testing

--- Makefile
+++ Makefile
@@ -11,24 +11,24 @@ lint:
 
 .PHONY: test
 test: ./go/go
-	@cargo build --release
-	@./scripts/test.sh release
+	cargo build --release
+	./scripts/test.sh release
 
-.PHONY: dev
-dev: ./go/go
-	@watchexec --restart --clear 'cargo build && ./scripts/test.sh debug'
+.PHONY: tdd
+tdd: ./go/go
+	cargo build && ./scripts/test.sh debug tdd
 
-.PHONY: dev-last
-dev-last: ./go/go
-	@watchexec --restart --clear 'cargo build && ./scripts/test.sh debug last'
+.PHONY: last
+last: ./go/go
+	cargo build && ./scripts/test.sh debug last
 
-.PHONY: debug-last
-debug-last:
-	@watchexec --restart --clear 'RUST_LOG=debug cargo run -- tokens $(shell cat .tests/_last)'
+.PHONY: last-debug
+last-debug:
+	RUST_LOG=debug cargo run -- tokens $(shell cat .tests/_last)
 
 .PHONY: trace-last
-trace-last:
-	@watchexec --restart --clear 'RUST_LOG=trace cargo run -- tokens $(shell cat .tests/_last)'
+last-trace:
+	RUST_LOG=trace cargo run -- tokens $(shell cat .tests/_last)
 
 ./go/go:
 	cd ./go && go build -o go .
--- scripts/test.sh
+++ scripts/test.sh
@@ -13,41 +13,59 @@ rm -rf ".tests/"
 mkdir ".tests/"
 
 # define the go files we want to test against
-if [ "$2" = 'last' ]; then
-  echo "$last" >.tests/_index
-else
-  for dir in tests .repositories; do
-    find "$dir" -type f -name '*.go' | sort > .tests/_index
-  done
-fi
+case "$2" in
+  'tdd')
+    for dir in tests; do
+      find "$dir" -type f -name '*.go' >> .tests/_index
+    done
+    ;;
+  'last')
+    echo "$last" >> .tests/_index
+    ;;
+  *)
+    for dir in tests .repositories; do
+      find "$dir" -type f -name '*.go' >> .tests/_index
+    done
+    ;;
+esac
 
-# keep track of how far we are
+# keep track of progress
 i=0
 total="$(wc -l < .tests/_index)"
 
-# compare tokens generated with the Go/Rust implementation
+# compare outputs from the Go/Rust implementation
 while read go_source; do
   i=$((i+1))
 
   echo "$go_source" > .tests/_last
   printf "%0.2d%% %s\n" "$((i*100/$total))" "$go_source"
 
+  # compare tokens outputs
+
   go_tokens=".tests/${go_source//\//--}.tokens-go"
-  "$GO_BIN" tokens "$go_source" > "$go_tokens" || {
-    echo "$go_source" >> .tests/_failures
-    continue
-  }
+  "$GO_BIN" tokens "$go_source" > "$go_tokens" || { echo "$go_source" >> .tests/_skipped; continue; }
 
   rust_tokens=".tests/${go_source//\//--}.tokens-rust"
   "$RUST_BIN" tokens "$go_source" > "$rust_tokens" || exit 1
 
   git diff --no-index "$go_tokens" "$rust_tokens" || exit 2
+
+  # compare ast outputs
+
+  go_ast=".tests/${go_source//\//--}.ast-go"
+  "$GO_BIN" ast "$go_source" > "$go_ast" || exit 3
+
+  rust_ast=".tests/${go_source//\//--}.ast-rust"
+  "$RUST_BIN" ast "$go_source" > "$rust_ast" || exit 4
+
+  git diff --no-index "$go_ast" "$rust_ast" || exit 5
+
 done < .tests/_index
 
 echo 'Success!'
 
-if [ -r .tests/_failures ]; then
+if [ -r .tests/_skipped ]; then
   echo
-  echo 'WARNING: these files were skipped as the Go parser failed to process them:'
-  cat .tests/_failures
+  echo 'WARNING: these files were skipped as Go failed to process them:'
+  cat .tests/_skipped
 fi
