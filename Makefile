7d24c6eccfd1c998a1f453b6602032f43c370d5a
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-11-14 01:09:59 +0100

    improve test script

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+CARGO_TEST_BIN_ARGS := --nocapture --test-threads=1
+
 .PHONY: all
 all: lint test
 
@@ -7,20 +9,20 @@ lint:
 
 .PHONY: test
 test:
-	cargo test --release -- --nocapture
+	cargo test --release -- $(CARGO_TEST_BIN_ARGS)
 
 .PHONY: test-lexer
 test-lexer:
-	cargo test --release lexer -- --nocapture
+	cargo test --release lexer -- $(CARGO_TEST_BIN_ARGS)
 
 .PHONY: test-parser
 test-parser:
-	cargo test --release parser -- --nocapture
+	cargo test --release parser -- $(CARGO_TEST_BIN_ARGS)
 
 .PHONY: dev-lexer
 dev-lexer:
-	watchexec --restart --clear 'DEV=true cargo test lexer -- --nocapture'
+	watchexec --restart --clear 'DEV=true cargo test lexer -- $(CARGO_TEST_BIN_ARGS)'
 
 .PHONY: dev-parser
 dev-parser:
-	watchexec --restart --clear 'DEV=true cargo test parser -- --nocapture'
+	watchexec --restart --clear 'DEV=true cargo test parser -- $(CARGO_TEST_BIN_ARGS)'
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,6 @@
 use crossbeam::thread;
 use glob::glob;
+use phf::{phf_set, Set};
 use std::env;
 use std::path::Path;
 use std::process::{Command, Output};
@@ -15,23 +16,23 @@ fn test_parser() {
 }
 
 fn run(command: &str) {
-    let (is_dev, rust_build_flags, go_pattern, go_bin, rust_bin, thread_count) =
+    let (is_dev, go_patterns, rust_build_flags, rust_bin, go_bin, thread_count) =
         if env::var("DEV").unwrap_or(String::from("dev")) == "true" {
             (
                 true,
+                vec!["tests/files/**/*.go"],
                 vec!["build"],
-                "tests/files/**/*.go",
-                "tests/go-cli/go-cli",
                 "target/debug/gors",
+                "tests/go-cli/go-cli",
                 1,
             )
         } else {
             (
                 false,
+                vec!["tests/files/**/*.go", ".repositories/**/*.go"],
                 vec!["build", "--release"],
-                ".repositories/**/*.go",
-                "tests/go-cli/go-cli",
                 "target/release/gors",
+                "tests/go-cli/go-cli",
                 8 * num_cpus::get(),
             )
         };
@@ -48,15 +49,25 @@ fn run(command: &str) {
     exec("cargo", &rust_build_flags).unwrap();
 
     println!("| finding go files...");
-    let go_files: Vec<_> = glob(go_pattern)
-        .unwrap()
-        .map(|entry| entry.unwrap().to_str().unwrap().to_owned())
+    let go_files: Vec<_> = go_patterns
+        .iter()
+        .flat_map(|pattern| {
+            glob(pattern).unwrap().filter_map(|entry| {
+                let path = entry.unwrap();
+                let path = path.to_str().unwrap();
+                if IGNORE_FILES.contains(path) {
+                    None
+                } else {
+                    Some(path.to_owned())
+                }
+            })
+        })
         .collect();
 
     let total = go_files.len();
     let chunk_size = (total / thread_count) + 1;
     println!(
-        "| starting {} thread(s) to test on {} go files in chunks of {}",
+        "| starting {} thread(s) to test {} go files in chunks of {}",
         thread_count, total, chunk_size,
     );
 
@@ -68,18 +79,15 @@ fn run(command: &str) {
                         println!("> {}", go_file);
                     }
 
-                    match exec(go_bin, &[command, go_file]) {
-                        Ok(go_output) => {
-                            let rust_output = exec(rust_bin, &[command, go_file]).unwrap();
-                            if go_output.stdout != rust_output.stdout {
-                                if is_dev {
-                                    // git diff
-                                } else {
-                                    panic!("Rust/Go outputs diff on: {:?}", go_file)
-                                }
-                            }
+                    let go_output = exec(go_bin, &[command, go_file]).unwrap();
+                    let rust_output = exec(rust_bin, &[command, go_file]).unwrap();
+
+                    if go_output.stdout != rust_output.stdout {
+                        if is_dev {
+                            // git diff
+                        } else {
+                            panic!("Rust/Go outputs diff on: {:?}", go_file)
                         }
-                        Err(err) => println!("Skipping file: {}, because {:?}", go_file, err),
                     }
                 }
             });
@@ -97,3 +105,55 @@ fn exec(bin: &str, args: &[&str]) -> Result<Output, Box<dyn std::error::Error>> 
 
     Ok(output)
 }
+
+// Some files cannot successfully be parsed by the Go compiler. So we exclude them from the
+// testing/benchmarking for now.
+static IGNORE_FILES: Set<&'static str> = phf_set! {
+    ".repositories/github.com/golang/go/src/cmd/api/testdata/src/pkg/p4/p4.go",
+    ".repositories/github.com/golang/go/src/constraints/constraints.go",
+    ".repositories/github.com/golang/go/src/go/doc/testdata/generics.go",
+    ".repositories/github.com/golang/go/src/go/parser/testdata/issue42951/not_a_file.go",
+    ".repositories/github.com/golang/go/test/bombad.go",
+    ".repositories/github.com/golang/go/test/char_lit1.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/bug014.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/bug068.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/bug163.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/bug169.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue11359.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue11610.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue15611.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue23587.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue30722.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue32133.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue4405.go",
+    ".repositories/github.com/golang/go/test/fixedbugs/issue9036.go",
+    ".repositories/github.com/golang/go/test/typeparam/absdiff.go",
+    ".repositories/github.com/golang/go/test/typeparam/absdiffimp.dir/a.go",
+    ".repositories/github.com/golang/go/test/typeparam/append.go",
+    ".repositories/github.com/golang/go/test/typeparam/boundmethod.go",
+    ".repositories/github.com/golang/go/test/typeparam/builtins.go",
+    ".repositories/github.com/golang/go/test/typeparam/double.go",
+    ".repositories/github.com/golang/go/test/typeparam/fact.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue39755.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue48137.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue48424.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue48453.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue48538.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue48609.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue48711.go",
+    ".repositories/github.com/golang/go/test/typeparam/issue49295.go",
+    ".repositories/github.com/golang/go/test/typeparam/list.go",
+    ".repositories/github.com/golang/go/test/typeparam/listimp.dir/a.go",
+    ".repositories/github.com/golang/go/test/typeparam/min.go",
+    ".repositories/github.com/golang/go/test/typeparam/minimp.dir/a.go",
+    ".repositories/github.com/golang/go/test/typeparam/nested.go",
+    ".repositories/github.com/golang/go/test/typeparam/ordered.go",
+    ".repositories/github.com/golang/go/test/typeparam/orderedmap.go",
+    ".repositories/github.com/golang/go/test/typeparam/orderedmapsimp.dir/a.go",
+    ".repositories/github.com/golang/go/test/typeparam/settable.go",
+    ".repositories/github.com/golang/go/test/typeparam/sliceimp.dir/a.go",
+    ".repositories/github.com/golang/go/test/typeparam/sliceimp.dir/main.go",
+    ".repositories/github.com/golang/go/test/typeparam/slices.go",
+    ".repositories/github.com/golang/go/test/typeparam/smallest.go",
+    ".repositories/github.com/golang/go/test/typeparam/typelist.go",
+};
