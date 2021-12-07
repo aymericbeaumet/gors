0ab6944caa7d3d75e6564b1f82b92744ae89f343
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-12-07 17:42:21 +0100

    [cli] work on run command

--- gors-cli/main.rs
+++ gors-cli/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::io::Write;
+use std::process::Command;
 
 #[derive(Parser)]
 #[clap(version = "1.0", author = "Aymeric Beaumet <hi@aymericbeaumet.com>")]
@@ -25,10 +26,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let opts: Opts = Opts::parse();
     match opts.subcmd {
-        SubCommand::Ast(cmd) => ast(cmd),
         SubCommand::Build(cmd) => build(cmd),
-        SubCommand::Tokens(cmd) => tokens(cmd),
         SubCommand::Run(cmd) => run(cmd),
+        SubCommand::Ast(cmd) => ast(cmd),
+        SubCommand::Tokens(cmd) => tokens(cmd),
     }
 }
 
@@ -38,41 +39,10 @@ struct Ast {
     filepath: String,
 }
 
-fn ast(cmd: Ast) -> Result<(), Box<dyn std::error::Error>> {
-    let stdout = std::io::stdout();
-    let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
-
-    let arena = gors::parser::Arena::new();
-    let filepath = cmd.filepath;
-    let buffer = std::fs::read_to_string(&filepath)?;
-
-    let file = gors::parser::parse_file(&arena, &filepath, &buffer)?;
-    gors::ast::fprint(&mut w, file)?;
-
-    w.flush()?;
-    Ok(())
-}
-
 #[derive(Parser)]
 struct Build {
     #[clap(name = "file", about = "The file to build")]
     filepath: String,
-}
-
-fn build(cmd: Build) -> Result<(), Box<dyn std::error::Error>> {
-    let stdout = std::io::stdout();
-    let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
-
-    let arena = gors::parser::Arena::new();
-    let filepath = cmd.filepath;
-    let buffer = std::fs::read_to_string(&filepath)?;
-
-    let parsed = gors::parser::parse_file(&arena, &filepath, &buffer)?;
-    let compiled = gors::compiler::compile(parsed)?;
-    gors::codegen::fprint(&mut w, compiled)?;
-
-    w.flush()?;
-    Ok(())
 }
 
 #[derive(Parser)]
@@ -81,24 +51,77 @@ struct Run {
     filepath: String,
 }
 
-fn run(_cmd: Run) -> Result<(), Box<dyn std::error::Error>> {
-    Ok(())
-}
-
 #[derive(Parser)]
 struct Tokens {
     #[clap(name = "file", about = "The file to lex")]
     filepath: String,
 }
 
+fn ast(cmd: Ast) -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = std::io::stdout();
+    let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
+
+    let arena = gors::parser::Arena::new();
+    let buffer = std::fs::read_to_string(&cmd.filepath)?;
+    let file = gors::parser::parse_file(&arena, &cmd.filepath, &buffer)?;
+    gors::ast::fprint(&mut w, file)?;
+
+    w.flush()?;
+    Ok(())
+}
+
+fn build(cmd: Build) -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = std::io::stdout();
+    let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
+
+    let arena = gors::parser::Arena::new();
+    let buffer = std::fs::read_to_string(&cmd.filepath)?;
+    let parsed = gors::parser::parse_file(&arena, &cmd.filepath, &buffer)?;
+    let compiled = gors::compiler::compile(parsed)?;
+    gors::codegen::fprint(&mut w, compiled)?;
+
+    w.flush()?;
+    Ok(())
+}
+
+fn run(cmd: Run) -> Result<(), Box<dyn std::error::Error>> {
+    let dir = tempdir::TempDir::new("gors")?;
+    let main_file = dir.path().join("main.rs");
+    let bin_file = dir.path().join("main");
+    let mut w = std::fs::File::create(&main_file)?;
+
+    let arena = gors::parser::Arena::new();
+    let buffer = std::fs::read_to_string(&cmd.filepath)?;
+    let parsed = gors::parser::parse_file(&arena, &cmd.filepath, &buffer)?;
+    let compiled = gors::compiler::compile(parsed)?;
+    gors::codegen::fprint(&mut w, compiled)?;
+    w.sync_all()?;
+
+    let output = Command::new("rustc")
+        .args([
+            main_file.to_str().unwrap(),
+            "-o",
+            bin_file.to_str().unwrap(),
+        ])
+        .output()?;
+    if !output.status.success() {
+        print!("{}", String::from_utf8_lossy(&output.stdout));
+        eprint!("{}", String::from_utf8_lossy(&output.stderr));
+        return Ok(());
+    }
+
+    let output = Command::new(&bin_file).output()?;
+    print!("{}", String::from_utf8_lossy(&output.stdout));
+    eprint!("{}", String::from_utf8_lossy(&output.stderr));
+    Ok(())
+}
+
 fn tokens(cmd: Tokens) -> Result<(), Box<dyn std::error::Error>> {
     let stdout = std::io::stdout();
     let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
 
-    let filepath = cmd.filepath;
-    let buffer = std::fs::read_to_string(&filepath)?;
-
-    let mut s = gors::scanner::Scanner::new(&filepath, &buffer);
+    let buffer = std::fs::read_to_string(&cmd.filepath)?;
+    let mut s = gors::scanner::Scanner::new(&cmd.filepath, &buffer);
 
     loop {
         let (pos, tok, lit) = s.scan()?;
