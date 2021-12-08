2a10eabc068d7ffdcc3f14a20af07ea3909e4c67
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-12-08 16:50:07 +0100

    cli improvements

--- gors-cli/main.rs
+++ gors-cli/main.rs
@@ -9,14 +9,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     match opts.subcmd {
         SubCommand::Ast(cmd) => ast(cmd),
         SubCommand::Build(cmd) => build(cmd),
-        SubCommand::Compile(cmd) => compile(cmd),
         SubCommand::Run(cmd) => run(cmd),
         SubCommand::Tokens(cmd) => tokens(cmd),
     }
 }
 
 #[derive(Parser)]
-#[clap(version = "1.0", author = "Aymeric Beaumet <hi@aymericbeaumet.com>")]
+#[clap(
+    version = "1.0",
+    name = "gors",
+    author = "Aymeric Beaumet <hi@aymericbeaumet.com>",
+    about = "gors is a go toolbelt written in rust; providing a parser and rust transpiler"
+)]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
@@ -26,10 +30,8 @@ struct Opts {
 enum SubCommand {
     #[clap(about = "Parse the named Go file and print the AST")]
     Ast(Ast),
-    #[clap(about = "Compile the named Go file and build an executable")]
+    #[clap(about = "Compile the named Go file")]
     Build(Build),
-    #[clap(about = "Compile the named Go file and print the generated Rust code")]
-    Compile(Compile),
     #[clap(about = "Compile and run the named Go file")]
     Run(Run),
     #[clap(about = "Scan the named Go file and print the tokens")]
@@ -39,39 +41,51 @@ enum SubCommand {
 #[derive(Parser)]
 struct Ast {
     #[clap(name = "file", about = "The file to parse")]
-    filepath: String,
+    file: String,
 }
 
 #[derive(Parser)]
 struct Build {
     #[clap(name = "file", about = "The file to build")]
-    filepath: String,
-}
-
-#[derive(Parser)]
-struct Compile {
-    #[clap(name = "file", about = "The file to compile")]
-    filepath: String,
+    file: String,
+    #[clap(
+        long,
+        name = "release",
+        about = "Build in release mode, with optimizations"
+    )]
+    release: bool,
+    #[clap(
+        long,
+        name = "emit",
+        about = "Type of output for the compiler to emit:\nrust|asm|llvm-bc|llvm-ir|obj|metadata|link|dep-info|mir"
+    )]
+    emit: Option<String>,
 }
 
 #[derive(Parser)]
 struct Run {
     #[clap(name = "file", about = "The file to run")]
-    filepath: String,
+    file: String,
+    #[clap(
+        long,
+        name = "release",
+        about = "Build in release mode, with optimizations"
+    )]
+    release: bool,
 }
 
 #[derive(Parser)]
 struct Tokens {
     #[clap(name = "file", about = "The file to lex")]
-    filepath: String,
+    file: String,
 }
 
 fn ast(cmd: Ast) -> Result<(), Box<dyn std::error::Error>> {
     let stdout = std::io::stdout();
     let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
 
-    let buffer = std::fs::read_to_string(&cmd.filepath)?;
-    let ast = gors::parser::parse_file(&cmd.filepath, &buffer)?;
+    let buffer = std::fs::read_to_string(&cmd.file)?;
+    let ast = gors::parser::parse_file(&cmd.file, &buffer)?;
     gors::ast::fprint(&mut w, ast)?;
     w.flush()?;
 
@@ -79,69 +93,59 @@ fn ast(cmd: Ast) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn build(cmd: Build) -> Result<(), Box<dyn std::error::Error>> {
-    let dir = tempdir::TempDir::new("gors")?;
-    let main_file = dir.path().join("main.rs");
-    let mut w = std::fs::File::create(&main_file)?;
-
-    let buffer = std::fs::read_to_string(&cmd.filepath)?;
-    let ast = gors::parser::parse_file(&cmd.filepath, &buffer)?;
+    let buffer = std::fs::read_to_string(&cmd.file)?;
+    let ast = gors::parser::parse_file(&cmd.file, &buffer)?;
     let compiled = gors::compiler::compile(ast)?;
-    gors::codegen::fprint(&mut w, compiled)?;
+
+    // shortcut when rust code is to be emitted
+    if matches!(cmd.emit.as_deref(), Some("rust")) {
+        let mut w = std::fs::File::create("main.rs")?;
+        gors::codegen::fprint(&mut w, compiled, true)?;
+        return Ok(());
+    }
+
+    let tmp_dir = tempdir::TempDir::new("gors")?;
+    let source_file = tmp_dir.path().join("main.rs");
+    let mut w = std::fs::File::create(&source_file)?;
+    gors::codegen::fprint(&mut w, compiled, false)?;
     w.sync_all()?;
 
     let rustc = Command::new("rustc")
-        .args([
-            main_file.to_str().unwrap(),
-            "--edition=2021",
-            "-Ccodegen-units=1",
-            "-Clto=fat",
-            "-Copt-level=3",
-            "-Ctarget-cpu=native",
-            "-o",
-            "main",
-        ])
+        .args(RustcArgs {
+            src: source_file.to_str().unwrap(),
+            out: None,
+            emit: cmd.emit.as_deref(),
+            release: cmd.release,
+        })
         .output()?;
     if !rustc.status.success() {
         print!("{}", String::from_utf8_lossy(&rustc.stdout));
         eprint!("{}", String::from_utf8_lossy(&rustc.stderr));
         return Ok(());
     }
-
-    Ok(())
-}
-
-fn compile(cmd: Compile) -> Result<(), Box<dyn std::error::Error>> {
-    let stdout = std::io::stdout();
-    let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
-
-    let buffer = std::fs::read_to_string(&cmd.filepath)?;
-    let ast = gors::parser::parse_file(&cmd.filepath, &buffer)?;
-    let compiled = gors::compiler::compile(ast)?;
-    gors::codegen::fprint(&mut w, compiled)?;
-    w.flush()?;
 
     Ok(())
 }
 
 fn run(cmd: Run) -> Result<(), Box<dyn std::error::Error>> {
-    let dir = tempdir::TempDir::new("gors")?;
-    let main_file = dir.path().join("main.rs");
-    let bin_file = dir.path().join("main");
-    let mut w = std::fs::File::create(&main_file)?;
+    let tmp_dir = tempdir::TempDir::new("gors")?;
+    let out_rust = tmp_dir.path().join("main.rs");
+    let out_bin = tmp_dir.path().join("main");
+    let mut w = std::fs::File::create(&out_rust)?;
 
-    let buffer = std::fs::read_to_string(&cmd.filepath)?;
-    let ast = gors::parser::parse_file(&cmd.filepath, &buffer)?;
+    let buffer = std::fs::read_to_string(&cmd.file)?;
+    let ast = gors::parser::parse_file(&cmd.file, &buffer)?;
     let compiled = gors::compiler::compile(ast)?;
-    gors::codegen::fprint(&mut w, compiled)?;
+    gors::codegen::fprint(&mut w, compiled, false)?;
     w.sync_all()?;
 
     let rustc = Command::new("rustc")
-        .args([
-            main_file.to_str().unwrap(),
-            "--edition=2021",
-            "-o",
-            bin_file.to_str().unwrap(),
-        ])
+        .args(RustcArgs {
+            src: out_rust.to_str().unwrap(),
+            out: Some(out_bin.to_str().unwrap()),
+            emit: None,
+            release: cmd.release,
+        })
         .output()?;
     if !rustc.status.success() {
         print!("{}", String::from_utf8_lossy(&rustc.stdout));
@@ -149,7 +153,7 @@ fn run(cmd: Run) -> Result<(), Box<dyn std::error::Error>> {
         return Ok(());
     }
 
-    let mut cmd = Command::new(&bin_file).spawn()?;
+    let mut cmd = Command::new(&out_bin).spawn()?;
     cmd.wait()?;
 
     Ok(())
@@ -159,12 +163,47 @@ fn tokens(cmd: Tokens) -> Result<(), Box<dyn std::error::Error>> {
     let stdout = std::io::stdout();
     let mut w = std::io::BufWriter::with_capacity(8192, stdout.lock());
 
-    let buffer = std::fs::read_to_string(&cmd.filepath)?;
-    for step in gors::scanner::Scanner::new(&cmd.filepath, &buffer) {
+    let buffer = std::fs::read_to_string(&cmd.file)?;
+    for step in gors::scanner::Scanner::new(&cmd.file, &buffer) {
         serde_json::to_writer(&mut w, &step?)?;
         w.write_all(b"\n")?;
     }
     w.flush()?;
 
     Ok(())
+}
+
+struct RustcArgs<'a> {
+    src: &'a str,
+    out: Option<&'a str>,
+    emit: Option<&'a str>,
+    release: bool,
+}
+
+impl<'a> IntoIterator for RustcArgs<'a> {
+    type Item = &'a str;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let mut out = vec![self.src, "--edition=2021"];
+
+        if let Some(e) = self.emit {
+            out.extend(["--emit", e]);
+        }
+
+        if let Some(o) = self.out {
+            out.extend(["-o", o]);
+        }
+
+        if self.release {
+            out.extend([
+                "-Ccodegen-units=1",
+                "-Clto=fat",
+                "-Copt-level=3",
+                "-Ctarget-cpu=native",
+            ]);
+        }
+
+        out.into_iter()
+    }
 }
--- gors/codegen/mod.rs
+++ gors/codegen/mod.rs
@@ -1,8 +1,49 @@
+use std::fmt;
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
+
+#[derive(Debug)]
+pub enum CodegenError {
+    Rustfmt(String),
+}
+
+impl std::error::Error for CodegenError {}
+
+impl fmt::Display for CodegenError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "codegen error: {:?}", self)
+    }
+}
+
 pub fn fprint<W: std::io::Write>(
     mut w: W,
     file: syn::File,
+    rustfmt: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let out = quote::quote! {#file};
-    w.write_all(out.to_string().as_bytes())?;
+    let ugly = (quote::quote! { #file }).to_string();
+    if !rustfmt {
+        w.write_all(ugly.as_bytes())?;
+        return Ok(());
+    }
+
+    let mut cmd = Command::new("rustfmt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+    let stdin = cmd.stdin.as_mut().unwrap();
+    stdin.write_all(ugly.as_bytes())?;
+
+    let output = cmd.wait_with_output()?;
+    if !output.status.success() {
+        return Err(Box::new(CodegenError::Rustfmt(
+            String::from_utf8(output.stderr).unwrap(),
+        )));
+    }
+
+    w.write_all(&output.stdout)?;
+
     Ok(())
 }
