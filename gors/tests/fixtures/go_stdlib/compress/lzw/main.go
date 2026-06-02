fe2908bda99d20522d175e3bb1ebdf35bb10a0cc
Aymeric Beaumet <aymeric@beaumet.me>
2026-06-02 09:58:30 +0200

    test: cover lzw order constants

--- gors/tests/fixtures/go_stdlib/compress/lzw/main.go
+++ gors/tests/fixtures/go_stdlib/compress/lzw/main.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 )
 
-func main() {
+func caseLzwOrder() {
+	// gors:stdlib-cover compress/lzw::LSB
+	// gors:stdlib-cover compress/lzw::MSB
+	// gors:stdlib-cover compress/lzw::Order
 	fmt.Println(lzw.LSB == lzw.LSB, lzw.LSB == lzw.MSB)
+}
+
+func main() {
+	caseLzwOrder()
 }
