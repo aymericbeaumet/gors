02a901eb9ca3a5d63db4f5a30a87335b985bab8e
Aymeric Beaumet <aymeric@beaumet.me>
2026-06-02 09:56:50 +0200

    test: cover gzip constants and header

--- gors/tests/fixtures/go_stdlib/compress/gzip/main.go
+++ gors/tests/fixtures/go_stdlib/compress/gzip/main.go
@@ -5,6 +5,30 @@ import (
 	"fmt"
 )
 
+func caseGzipConstants() {
+	// gors:stdlib-cover compress/gzip::BestCompression
+	// gors:stdlib-cover compress/gzip::BestSpeed
+	// gors:stdlib-cover compress/gzip::DefaultCompression
+	// gors:stdlib-cover compress/gzip::HuffmanOnly
+	// gors:stdlib-cover compress/gzip::NoCompression
+	fmt.Println(gzip.NoCompression, gzip.BestSpeed, gzip.BestCompression, gzip.DefaultCompression, gzip.HuffmanOnly)
+}
+
+func caseGzipErrors() {
+	// gors:stdlib-cover compress/gzip::ErrChecksum
+	fmt.Println(gzip.ErrChecksum.Error())
+	// gors:stdlib-cover compress/gzip::ErrHeader
+	fmt.Println(gzip.ErrHeader.Error())
+}
+
+func caseGzipHeader() {
+	// gors:stdlib-cover compress/gzip::Header
+	h := gzip.Header{Name: "name.txt", Comment: "fixture", OS: 3}
+	fmt.Println(h.Name, h.Comment, h.OS)
+}
+
 func main() {
-	fmt.Println(gzip.NoCompression)
+	caseGzipConstants()
+	caseGzipErrors()
+	caseGzipHeader()
 }
