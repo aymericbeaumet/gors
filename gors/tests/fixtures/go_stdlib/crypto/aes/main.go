b71ccca748e90bc2986df550010005d04ab0d81c
Aymeric Beaumet <aymeric@beaumet.me>
2026-06-02 10:13:29 +0200

    test: cover aes constants and errors

--- gors/tests/fixtures/go_stdlib/crypto/aes/main.go
+++ gors/tests/fixtures/go_stdlib/crypto/aes/main.go
@@ -5,7 +5,14 @@ import (
 	"fmt"
 )
 
-func main() {
+func caseAesConstantsAndErrors() {
+	// gors:stdlib-cover crypto/aes::BlockSize
 	fmt.Println(aes.BlockSize)
+	// gors:stdlib-cover crypto/aes::KeySizeError
+	// gors:stdlib-cover crypto/aes::KeySizeError.Error
 	fmt.Println(aes.KeySizeError.Error(aes.KeySizeError(7)))
+}
+
+func main() {
+	caseAesConstantsAndErrors()
 }
