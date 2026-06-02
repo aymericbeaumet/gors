80821ba6322e0beb2704e16fe75f5a30add4bc12
Aymeric Beaumet <aymeric@beaumet.me>
2026-06-02 09:49:23 +0200

    test: mark covered cmp helpers

--- gors/tests/fixtures/go_stdlib/cmp/main.go
+++ gors/tests/fixtures/go_stdlib/cmp/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// gors:stdlib-cover cmp::Ordered
 func orderedMin[T cmp.Ordered](a T, b T) T {
 	if cmp.Less(b, a) {
 		return b
@@ -13,5 +14,8 @@ func orderedMin[T cmp.Ordered](a T, b T) T {
 }
 
 func main() {
+	// gors:stdlib-cover cmp::Compare
+	// gors:stdlib-cover cmp::Less
+	// gors:stdlib-cover cmp::Or
 	fmt.Println(cmp.Compare(2, 5), cmp.Compare("go", "rust"), cmp.Less(2.5, 7.5), cmp.Or(0, 5, 7), orderedMin(4, 2))
 }
