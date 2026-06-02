343878a07879e151a6e13d0cfa07041e201d3190
Aymeric Beaumet <aymeric@beaumet.me>
2026-06-02 22:18:09 +0200

    test: cover rsa options and audit key gaps

--- gors/tests/fixtures/go_stdlib/crypto/rsa/main.go
+++ gors/tests/fixtures/go_stdlib/crypto/rsa/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto"
 	"crypto/rsa"
 	"fmt"
 )
@@ -8,4 +9,18 @@ import (
 func main() {
 	// gors:stdlib-cover crypto/rsa::PSSSaltLengthAuto crypto/rsa::PSSSaltLengthEqualsHash
 	fmt.Println(rsa.PSSSaltLengthAuto, rsa.PSSSaltLengthEqualsHash)
+
+	// gors:stdlib-cover crypto/rsa::ErrDecryption crypto/rsa::ErrMessageTooLong crypto/rsa::ErrVerification
+	fmt.Println(rsa.ErrDecryption.Error())
+	fmt.Println(rsa.ErrMessageTooLong.Error())
+	fmt.Println(rsa.ErrVerification.Error())
+
+	// gors:stdlib-cover crypto/rsa::OAEPOptions crypto/rsa::PKCS1v15DecryptOptions
+	oaep := rsa.OAEPOptions{Hash: crypto.SHA256, MGFHash: crypto.SHA512, Label: []byte("label")}
+	pkcs := rsa.PKCS1v15DecryptOptions{SessionKeyLen: 16}
+	fmt.Println(oaep.Hash == crypto.SHA256, oaep.MGFHash == crypto.SHA512, string(oaep.Label), pkcs.SessionKeyLen)
+
+	// gors:stdlib-cover crypto/rsa::PSSOptions crypto/rsa::PSSOptions.HashFunc
+	pss := &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash, Hash: crypto.SHA256}
+	fmt.Println(pss.HashFunc() == crypto.SHA256, pss.SaltLength)
 }
