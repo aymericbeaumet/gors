1031564384e86993e5833514244c548046c22942
Aymeric Beaumet <aymeric@beaumet.me>
2026-06-02 11:06:39 +0200

    test: cover rc4 error types

--- gors/tests/fixtures/go_stdlib/crypto/rc4/main.go
+++ gors/tests/fixtures/go_stdlib/crypto/rc4/main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	fmt.Println("== rc4/basic ==")
+
+	// gors:stdlib-cover crypto/rc4::Cipher
 	var cipher *rc4.Cipher
 	fmt.Println(cipher == nil)
+
+	// gors:stdlib-cover crypto/rc4::KeySizeError crypto/rc4::KeySizeError.Error
 	fmt.Println(rc4.KeySizeError.Error(rc4.KeySizeError(2)))
 }
