d18cda10fcaa7b1f86783595f4dd2c41ead74944
Aymeric Beaumet <aymeric@beaumet.me>
2026-06-03 15:52:51 +0200

    refactor: isolate stdlib call workarounds

--- gors/src/compiler/mod.rs
+++ gors/src/compiler/mod.rs
@@ -17,8 +17,10 @@
 pub mod ir;
 pub mod manifest;
 pub(crate) mod passes;
+mod stdlib_workarounds;
 pub mod typeinfer;
 
+use crate::compiler::stdlib_workarounds::StdlibCallWorkaround;
 use crate::mapping::SourceMapTracker;
 use crate::{ast, token};
 use proc_macro2::Span;
@@ -15884,86 +15886,6 @@ fn compile_builtin_print(args: Vec<syn::Expr>) -> syn::Expr {
         .map(|arg| syn::parse_quote! { crate::builtin::print_value(#arg); })
         .collect::<Vec<syn::Stmt>>();
     syn::parse_quote! {{ #(#stmts)* }}
-}
-
-enum StdlibCallWorkaround {
-    SortSlice,
-}
-
-impl StdlibCallWorkaround {
-    fn classify(call_expr: &ast::CallExpr<'_>) -> Option<Self> {
-        if is_sort_slice_call(call_expr) {
-            return Some(Self::SortSlice);
-        }
-        None
-    }
-
-    fn compile(self, call_expr: ast::CallExpr<'_>) -> syn::Expr {
-        match self {
-            Self::SortSlice => compile_sort_slice_call(call_expr)
-                .unwrap_or_else(|| compile_error_expr("invalid sort slice call")),
-        }
-    }
-}
-
-fn compile_sort_slice_call(call_expr: ast::CallExpr) -> Option<syn::Expr> {
-    let ast::Expr::SelectorExpr(selector) = *call_expr.fun else {
-        return None;
-    };
-    if !matches!(*selector.x, ast::Expr::Ident(pkg) if pkg.name == "sort") {
-        return None;
-    }
-    if !matches!(selector.sel.name, "Slice" | "SliceStable" | "SliceIsSorted") {
-        return None;
-    }
-
-    let mut args = call_expr.args.unwrap_or_default().into_iter();
-    let ast::Expr::Ident(slice_ident) = args.next()? else {
-        return None;
-    };
-    let less_arg = args.next()?;
-    if args.next().is_some() {
-        return None;
-    }
-
-    let slice_ident = syn::Ident::new(&rust_safe_ident_name(slice_ident.name), Span::mixed_site());
-    let less: syn::Expr = less_arg.into();
-    match selector.sel.name {
-        "Slice" | "SliceStable" => Some(syn::parse_quote! {{
-            let mut __gors_less = #less;
-            let __gors_len = #slice_ident.len();
-            for __gors_i in 0..__gors_len {
-                for __gors_j in (__gors_i + 1)..__gors_len {
-                    if __gors_less(__gors_j as isize, __gors_i as isize) {
-                        #slice_ident.swap(__gors_i, __gors_j);
-                    }
-                }
-            }
-        }}),
-        "SliceIsSorted" => Some(syn::parse_quote! {{
-            let mut __gors_less = #less;
-            let mut __gors_sorted = true;
-            let __gors_len = #slice_ident.len();
-            let mut __gors_i = 1usize;
-            while __gors_i < __gors_len {
-                if __gors_less(__gors_i as isize, (__gors_i - 1) as isize) {
-                    __gors_sorted = false;
-                    break;
-                }
-                __gors_i += 1;
-            }
-            __gors_sorted
-        }}),
-        _ => None,
-    }
-}
-
-fn is_sort_slice_call(call_expr: &ast::CallExpr) -> bool {
-    let ast::Expr::SelectorExpr(selector) = &*call_expr.fun else {
-        return false;
-    };
-    matches!(&*selector.x, ast::Expr::Ident(pkg) if pkg.name == "sort")
-        && matches!(selector.sel.name, "Slice" | "SliceStable" | "SliceIsSorted")
 }
 
 fn ordered_builtin_arg_expected_type(raw_args: &[ast::Expr]) -> Option<typeinfer::GoType> {
